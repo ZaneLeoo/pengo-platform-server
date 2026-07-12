10d9c7faaee0e365cef1c6d57afeb2ffed010a84
ZaneLeoo <ZaneLeoo@users.noreply.github.com>
2026-07-12 13:58:26 +0800

    fix: 入库单表头仓库改为非必填，仓库由行级控制

--- ruoyi-mes/src/main/java/com/ruoyi/mes/purchase/domain/PurchaseInbound.java
+++ ruoyi-mes/src/main/java/com/ruoyi/mes/purchase/domain/PurchaseInbound.java
@@ -22,7 +22,6 @@ public class PurchaseInbound extends BaseEntity {
     private String inboundCode;
     @NotBlank(message = "入库日期不能为空")
     private String inboundDate;
-    @NotBlank(message = "入库仓库不能为空")
     private String warehouseCode;
     private String warehouseName;
     @NotBlank(message = "入库单状态不能为空")
